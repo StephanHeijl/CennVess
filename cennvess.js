1c6f8b38a4b29adf1adca075a0954f910445dc30
StephanHeijl <me@stephanheijl.com>
2013-12-01 22:55:08 +0100

    Code outline

---
+++ cennvess.js
@@ -0,0 +1,21 @@
+var CennVess = function( element ) {
+	this.element = element;
+	this.context = element.getContext("2d");
+	
+	// Draw Circle
+	
+	// Calculate relative size
+	
+	// Calculate relative area 
+	
+	// Calculate overlap size
+	
+	// Calculate overlap area
+	
+	// Calculate overlap position
+	
+	// Draw circles
+	
+	// Draw text 
+
+});
